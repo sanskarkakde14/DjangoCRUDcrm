c39de36e720d22bf29ef5d79a11763351162411b
Sanskar Kakde <80622561+sanskarkakde14@users.noreply.github.com>
2022-07-03 01:54:53 +0530

    Update views.py

--- djangoProject3/accounts/views.py
+++ djangoProject3/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,HttpResponse
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
